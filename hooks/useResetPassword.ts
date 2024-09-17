733b387d5a32d2c95d1cbeaf5f308682f9f3c650
JanBu <yosefsitumorang97@gmail.com>
2024-09-18 00:47:15 +0700

    feat:integration api reset password and forgot password

---
+++ hooks/useResetPassword.ts
@@ -0,0 +1,40 @@
+import { response } from '@/types/response'
+import { useState } from 'react'
+
+interface ResetPassword {
+  email: string
+  newPassword: string
+}
+
+const useResetPassword = () => {
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<unknown>(null)
+
+  const handleResetPassword = async (request: ResetPassword) => {
+    setLoading(true)
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_HOSTNAME_API}/${process.env.NEXT_PUBLIC_PREFIX_API}/auth/reset-password`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(request),
+          credentials: 'include',
+        }
+      )
+      const data: response = await response.json()
+      setLoading(false)
+
+      return data
+    } catch (err) {
+      setError(err)
+      console.error('Reset password error:', error)
+    }
+    setLoading(false)
+  }
+
+  return { handleResetPassword, loading, error }
+}
+export default useResetPassword
