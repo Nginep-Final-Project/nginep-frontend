623966e2250a4e632780ff93872058e9552677db
JanBu <yosefsitumorang97@gmail.com>
2024-10-03 00:37:16 +0700

    feat: integration api property list tenant and delete property

--- hooks/usePropertyManagement.ts
+++ hooks/usePropertyManagement.ts
@@ -1,3 +1,4 @@
+import { toast } from '@/components/ui/use-toast'
 import { CreateProperty } from '@/types/createProperty'
 import { response } from '@/types/response'
 import { useState } from 'react'
@@ -31,8 +32,44 @@ const usePropertyManagement = () => {
     setLoading(false)
   }
 
+  const handleDeleteProperty = async (propertyId: number) => {
+    setLoading(true)
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_HOSTNAME_API}/${process.env.NEXT_PUBLIC_PREFIX_API}/property/${propertyId}`,
+        {
+          method: 'DELETE',
+          credentials: 'include',
+        }
+      )
+
+      const data: response = await response.json()
+      if (!data.success) {
+        toast({
+          variant: 'destructive',
+          title: data.message,
+        })
+        return
+      }
+      setLoading(false)
+      toast({
+        title: data.message,
+      })
+      return data
+    } catch (error) {
+      setError(error)
+      toast({
+        variant: 'destructive',
+        title: 'Delete property failed',
+      })
+      console.error('Delete property error:', error)
+    }
+    setLoading(false)
+  }
+
   return {
     handleCreateProperty,
+    handleDeleteProperty,
     loading,
     error,
   }
