3b2a9046395b5465b773a2d22252edf75486e0d9
irfazbow <irfanefp@gmail.com>
2024-10-14 01:05:57 +0700

    feat: skeleton for dashboard reviews

--- app/dashboard/review/page.tsx
+++ app/dashboard/review/page.tsx
@@ -10,6 +10,8 @@ import {
 } from "@/components/ui/select";
 import PropertyReviews from "./_components/PropertyReviews";
 import { useUserProperties } from "@/hooks/property/useProperties.ts";
+import { Skeleton } from "@/components/ui/skeleton";
+import ReviewsSkeleton from "./_components/Skeleton/ReviewsSkeleton";
 
 const Review: React.FC = () => {
   const [selectedPropertyId, setSelectedPropertyId] = useState<number | null>(
@@ -18,7 +20,8 @@ const Review: React.FC = () => {
 
   const { data: properties, isLoading, error } = useUserProperties();
 
-  if (isLoading) return <div>Loading properties...</div>;
+  const noPropertiesFound = !properties || properties.length === 0;
+
   if (error) return <div>Error loading properties</div>;
 
   return (
@@ -27,21 +30,42 @@ const Review: React.FC = () => {
         Manage Property Reviews
       </h1>
       <div className="mb-10">
-        <Select onValueChange={(value) => setSelectedPropertyId(Number(value))}>
-          <SelectTrigger className="w-[220px] sm:w-[400px]">
-            <SelectValue placeholder="Select a property" />
-          </SelectTrigger>
-          <SelectContent className="bg-white">
-            {properties?.map((property) => (
-              <SelectItem key={property.id} value={property.id.toString()}>
-                {property.propertyName}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
+        {isLoading ? (
+          <Skeleton className="w-[220px] sm:w-[400px] h-10 bg-gray-200" />
+        ) : (
+          <Select
+            onValueChange={(value) => setSelectedPropertyId(Number(value))}
+          >
+            <SelectTrigger className="w-[220px] sm:w-[400px]">
+              <SelectValue placeholder="Select a property" />
+            </SelectTrigger>
+            <SelectContent className="bg-white">
+              {properties?.map((property) => (
+                <SelectItem key={property.id} value={property.id.toString()}>
+                  {property.propertyName}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        )}
       </div>
-      {selectedPropertyId && (
+      {selectedPropertyId ? (
         <PropertyReviews propertyId={selectedPropertyId} />
+      ) : noPropertiesFound ? (
+        <div
+          className="flex items-center p-4 mb-4 text-sm text-yellow-800 border border-yellow-300 rounded-lg bg-yellow-50"
+          role="alert"
+        >
+          <div>
+            <div className="font-medium mb-2">No properties found!</div>
+            <div className="text-justify">
+              You currently have no registered properties. Please add a property
+              to view its reviews.
+            </div>
+          </div>
+        </div>
+      ) : (
+        <ReviewsSkeleton />
       )}
     </div>
   );
